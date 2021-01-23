d82c61d5e470996bf7cc961f2f5c199f879b367c
Thomas Nelson <thnlsn@gmail.com>
2021-01-22 22:39:08 -0800

    removed selected text from settings because there is visual indicator on each of the buttons making it uneccesary

--- src/App.js
+++ src/App.js
@@ -76,46 +76,6 @@ function App() {
     setSelectedParty(selectedParty);
   };
 
-  const addPts = (points, selectedParty, clickedParty) => {
-    //// We need...
-    // the amount of points for the state clicked (so we know how much points to add/subtract)
-    // the party selected by user (the one we add points to)
-    // the party of the state currently clicked (so we can remove the points from it, or if it is equal to the selected party, set it to none (if it colored) else do nothing
-    switch (selectedParty) {
-      case 'blue':
-        const newBluePoints = bluePts + points;
-        setBluePts(newBluePoints);
-        break;
-      case 'red':
-        const newRedPoints = redPts + points;
-        setRedPts(newRedPoints);
-        break;
-      default:
-        const newNonePoints = nonePts + points;
-        setNonePts(newNonePoints);
-    }
-  };
-
-  /* 
-  
-  NONE SELECTED: CLICK NONE --> DO NOTHING
-  NONE SELECTED: CLICK BLUE --> [ADD POINTS TO NONE + ADD STATE TO NONE + SUBTRACT POINTS FROM BLUE + REMOVE STATE FROM BLUE]
-  NONE SELECTED: CLICK RED --> {ADD POINTS TO NONE + ADD STATE TO NONE + SUBTRACT POINTS FROM RED + REMOVE STATE FROM RED}
-
-  BLUE SELECTED: CLICK NONE --> ADD POINTS TO BLUE + ADD STATE TO BLUE + SUBTRACT POINTS FROM NONE + REMOVE STATE FROM NONE
-  BLUE SELECTED: CLICK BLUE --> [ADD POINTS TO NONE + ADD STATE TO NONE + SUBTRACT POINTS FROM BLUE + REMOVE STATE FROM BLUE]
-  BLUE SELECTED: CLICK RED --> ADD POINTS TO BLUE + ADD STATE TO BLUE + SUBTRACT POINTS FROM RED + REMOVE STATE FROM RED
-
-  RED SELECTED: CLICK NONE --> ADD POINTS TO RED + ADD STATE TO RED + SUBTRACT POINTS FROM NONE + REMOVE STATE FROM NONE
-  RED SELECTED: CLICK BLUE --> ADD POINTS TO RED + ADD STATE TO RED + SUBTRACT POINTS FROM BLUE + REMOVE STATE FROM BLUE
-  RED SELECTED: CLICK RED --> {ADD POINTS TO NONE + ADD STATE TO NONE + SUBTRACT POINTS FROM RED + REMOVE STATE FROM RED}
-
-if clicked and selected are same: set to none and if it was red, subtract from red and add to none, same for blue
-else, clicked and selected must be different:
-  - if clicked is none, minus
-
-  */
-
   const handleClick = (selected, clicked, state, points) => {
     switch (selected) {
       case 'blue':
@@ -177,35 +137,6 @@ else, clicked and selected must be different:
           setRedPts(redPts - points);
           setRedStates(redStates.filter((redState) => redState !== state));
         }
-    }
-  };
-
-  const addToParty = (selectedParty, clickedParty, state) => {
-    // If the state clicked is currently a none state AND the selected party is blue or red...
-    console.log(noneStates.includes(state) && selectedParty !== 'none');
-    // CLICKED ON NONE STATE AND EITHER BLUE OR RED IS SELECTED
-    if (noneStates.includes(state) && selectedParty !== 'none') {
-      if (selectedParty === 'blue') {
-        console.log(
-          `YOU HAVE ${selectedParty} SELECTED AND YOU CLICKED ON ${state}, WHICH IS A ${clickedParty} STATE`
-        );
-        // First add that state to the blueStates
-        setBlueStates([...blueStates, state]);
-        // Remove it from the noneStates by filtering the array so that it only includes items that do not match the state clicked
-        setNoneStates(noneStates.filter((noneState) => noneState !== state));
-        console.log(noneStates);
-      } else if (selectedParty === 'red') {
-        console.log(
-          `YOU HAVE ${selectedParty} SELECTED AND YOU CLICKED ON ${state}, WHICH IS A ${clickedParty} STATE`
-        );
-        // First add that state to the blueStates
-        setRedStates([...redStates, state]);
-        // Remove it from the noneStates by filtering the array so that it only includes items that do not match the state clicked
-        setNoneStates(noneStates.filter((noneState) => noneState !== state));
-        console.log(noneStates);
-      }
-      /*     // CLICKED ON RED OR BLUE STATE
-    } else if (!noneStates.includes(state)) */
     }
   };
 
