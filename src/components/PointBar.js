2268540f9215e89f86399c088ee6621aa0c571fa
Thomas Nelson <thnlsn@gmail.com>
2021-01-23 00:39:16 -0800

    added number indicator to the middle point bar as well as not displaying the number if its below 20 so that it doesnt appear overflowed

--- src/components/PointBar.js
+++ src/components/PointBar.js
@@ -8,9 +8,9 @@ const PointBar = ({ bluePts, redPts, nonePts }) => {
         gridTemplateColumns: `[bar-start blue-start] ${bluePts}fr [blue-end none-start] ${nonePts}fr [none-end red-start] ${redPts}fr [red-end bar-end]`,
       }}
     >
-      <div className='bar__blue' data-content={bluePts} />
-      <div className='bar__none' data-content={nonePts} />
-      <div className='bar__red' data-content={redPts} />
+      <div className='bar__blue' data-content={bluePts < 15 ? '' : bluePts} />
+      <div className='bar__none' data-content={nonePts < 15 ? '' : nonePts} />
+      <div className='bar__red' data-content={redPts < 15 ? '' : redPts} />
     </div>
   );
 };
