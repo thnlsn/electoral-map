aaa9e68822a311681c32682a1c0f8df28e1791bc
Thomas Nelson <thnlsn@gmail.com>
2021-01-25 19:47:17 -0800

    point visualizer working and changed the background to a plain white

--- src/components/UnitedStates.js
+++ src/components/UnitedStates.js
@@ -22,7 +22,9 @@ function UnitedStates({
       <g className='map'>
         {states.map((state, index) => (
           <path
-            className={`map__state ${
+            className={`map__state map__state--${
+              visualize ? 'visualize' : 'default'
+            } ${
               noneStates.includes(state.name)
                 ? 'none'
                 : blueStates.includes(state.name)
@@ -43,22 +45,31 @@ function UnitedStates({
             onMouseEnter={handleHover}
             onMouseLeave={handleHoverExit}
             style={
-              visualize
-                ? noneStates.includes(state.name)
+              visualize // If visualize is true, continue
+                ? noneStates.includes(state.name) // If the state is a noneState
+                  ? hovered === state.name
+                    ? {
+                        fill: `rgba(217, 200, 130, 0.5)`, // noneState color based on points and if it's hovered
+                      }
+                    : {
+                        fill: `rgba(217, 200, 130, calc(${state.points} / 46))`, // noneState color based on points
+                      }
+                  : blueStates.includes(state.name) // If the state is a blueState
+                  ? hovered === state.name
+                    ? {
+                        fill: `rgba(40, 44, 250, 0.5)`, // blueState color based on points and if it's hovered
+                      }
+                    : {
+                        fill: `rgba(40, 44, 250, calc(${state.points} / 46))`, // blueState color based on points
+                      }
+                  : hovered === state.name
                   ? {
-                      fill: `rgba(217, 200, 130, calc(${state.points} / 40))`, // Divide by 55 because 55 is the max points (CA) and that would equate to a 1.0 opacity
-                      stroke: '#988c5b',
-                    }
-                  : blueStates.includes(state.name)
-                  ? {
-                      fill: `rgba(40, 44, 250, calc(${state.points} / 40))`, // Divide by 55 because 55 is the max points (CA) and that would equate to a 1.0 opacity
-                      stroke: '#988c5b',
+                      fill: `rgba(173, 33, 73, 0.5)`, // redState color based on points and if it's hovered
                     }
                   : {
-                      fill: `rgba(173, 33, 73, calc(${state.points} / 40))`, // Divide by 55 because 55 is the max points (CA) and that would equate to a 1.0 opacity
-                      stroke: '#988c5b',
+                      fill: `rgba(173, 33, 73, calc(${state.points} / 46))`, // redState color based on points
                     }
-                : {}
+                : {} // If visualize is false, do nothing
             }
           ></path>
         ))}
@@ -68,6 +79,8 @@ function UnitedStates({
 }
 
 export default UnitedStates;
+
+//10.549019607843
 
 // Check if visualize is true
 //// If yes, check if state is blue or red currently
