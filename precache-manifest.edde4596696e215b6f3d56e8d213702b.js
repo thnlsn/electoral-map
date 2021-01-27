027f36320fa4fdab853c2d716e76c4d9bbacae58
Thomas Nelson <thnlsn@gmail.com>
2021-01-27 00:14:30 -0800

    Updates

---
+++ precache-manifest.edde4596696e215b6f3d56e8d213702b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a915e6623d4b345b8eec4999a39a0a25",
+    "url": "/electoral-map/index.html"
+  },
+  {
+    "revision": "e21a473b7880763c9aad",
+    "url": "/electoral-map/static/css/main.54f8b98c.chunk.css"
+  },
+  {
+    "revision": "cb0a924cd5a44e9ce666",
+    "url": "/electoral-map/static/js/2.fd6f1a5c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/electoral-map/static/js/2.fd6f1a5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e21a473b7880763c9aad",
+    "url": "/electoral-map/static/js/main.231f9221.chunk.js"
+  },
+  {
+    "revision": "3225b5ef81f0b16a7c05",
+    "url": "/electoral-map/static/js/runtime-main.422c4060.js"
+  }
+]);
